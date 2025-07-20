611aec17453bc11537f6ae50c87f98c194261687
Rahul Kumhar <rahulkumhar03@gmail.com>
2025-07-20 23:25:44 +0530

    Modified the vehicle model and landing page

--- Vehicle-Rental-Frontend/src/app/pages/landing-page/landing-page.component.ts
+++ Vehicle-Rental-Frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { CommonModule} from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { VehicleCardComponent } from '../vehicle-card/vehicle-card.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, VehicleCardComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
-
+  cardVehicles = [
+        { name: 'The Mercedes-AMG A 45 S', price: '150', image: 'assets/images/AMG-A45.png', bodyType: 'Hatchback', range: 2000 },
+        { name: 'BMW M5', price: '120', image: 'assets/images/BMW-M5.png', bodyType: 'Sedan', range:2200 },
+        { name: 'Audi R8', price: '130', image: 'assets/images/Audi-R8.png', bodyType: 'Sports', range: 1500 }
+    ];
 }
