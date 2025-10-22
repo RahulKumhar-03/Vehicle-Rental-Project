16fbbb79345d0b6523b7bb5ce68e141091b6a075
Rahul Kumhar <rahulkumhar03@gmail.com>
2025-10-22 18:25:49 +0530

    changes in frontend

--- Vehicle-Rental-Server/config/settings.py
+++ Vehicle-Rental-Server/config/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_url : str 
+    database_url : str = ""
     jwt_secret_key : str = ""
     algorithm : str 
 
