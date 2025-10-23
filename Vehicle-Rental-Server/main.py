70a9f59e2cda95ca3b283f63d7acd38d4dc83396
Rahul Kumhar <rahulkumhar03@gmail.com>
2025-10-23 13:07:09 +0530

    deployed frontend configuration

--- Vehicle-Rental-Server/main.py
+++ Vehicle-Rental-Server/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  
+    allow_origins=["https://skyline-rentals.onrender.com"],  
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"], 
     allow_headers=["*"],
